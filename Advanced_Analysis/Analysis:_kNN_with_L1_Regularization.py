4a9e4fc50265065085961dd8bdce2cd834272a63
Wally Wissner <wally.wissner@techfield.us>
2019-03-31 19:08:53 -0400

    Added comments.

--- Advanced_Analysis/Analysis:_kNN_with_L1_Regularization.py
+++ Advanced_Analysis/Analysis:_kNN_with_L1_Regularization.py
@@ -36,35 +36,40 @@ if __name__ == "__main__":
         Y[data] = df[data].as_matrix(columns=[target])
 
 
-    # Model.
-
+    # Hyperparameters.
     lambda1 = 1500
-    n = 3
+    k = 3
     norm = 1
     weights = "distance"
     threshold = .01
 
+    # Perform L1 Regularization on Linear Regression to reduce data dimensionality.
     m_linear = Lasso(alpha=lambda1)
     m_linear.fit(X["train"], Y["train"])
     r2 = m_linear.score(X["validate"], Y["validate"])
 
+    # Number of weights above threshold (including bias weight).
     count = len([w for w in m_linear.coef_ if w > threshold])
 
     print("linear regression: R^2={:0.4f}, lambda1={:0.4f}, weight_count={}"
           .format(r2, lambda1, len([w for w in m_linear.coef_ if w > threshold])))
 
+    # Indices of columns that survive dimensionality reduction.
     indices = [i for i, w in enumerate(m_linear.coef_) if w > threshold]
 
+    # Names of columns that survive dimensionality reduction.
     colnames = list(df["train"].drop(columns=target).columns[indices[1:]])
     print(colnames)
 
+    # Reduced matrixes.
     for data in df:
         X[data + "_L1"] = X[data][:,indices]
 
-    m_knn = KNeighborsRegressor(n_neighbors=n, p=norm, weights=weights)
+    # kNN Regression over reduced matrices.
+    m_knn = KNeighborsRegressor(n_neighbors=k, p=norm, weights=weights)
     m_knn.fit(X["train_L1"], Y["train"])
     r2 = m_knn.score(X["validate_L1"], Y["validate"])
-    print(f"kNN R^2={r2}, lambda1={lambda1}, count={count}, n={n}, norm={norm}, weights={weights}")
+    print(f"kNN R^2={r2}, lambda1={lambda1}, count={count}, k={k}, norm={norm}, weights={weights}")
 
 
 
